54c5fb3a6541f780db596aea846a5ffc4657f0e0
LG <LG@DESKTOP-SJJ1G31>
2018-01-02 12:33:50 +0900

    BTest 두번째 커밋 (a사용자) 출력

--- JAVA_GIT/src/ch01/BTest.java
+++ JAVA_GIT/src/ch01/BTest.java
@@ -3,5 +3,7 @@ package ch01;
 public class BTest {
 	public static void main(String[] args) {
 		System.out.println("BTest Ãâ·Â~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		System.out.println("BTest Ä¿¹Ô2~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+		
 	}
 }
